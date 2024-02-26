9040c0542b6686ec740dcf178b7354ebeb27ae7c
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-02-26 20:30:38 -0300

    Variables

---
+++ Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 1;
+        long x = 12323232323232L;
+        byte b = 110;
+        boolean c = true;
+        float d = 3.12F;
+        char e = 'A';
+        String f = "A";
+
+        System.out.println("My number is " + d);
+    }
+}
