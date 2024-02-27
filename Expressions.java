8c3798632f91243622248ea58df3b6990d3dfba9
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-02-27 20:03:10 -0300

    Expressions

---
+++ Expressions.java
@@ -0,0 +1,8 @@
+public class Expressions {
+    public static void main(String[] args) {
+        double friends = 10;
+        //friends++;
+        friends = (double) friends /3;
+        System.out.println(friends);
+    }
+}
