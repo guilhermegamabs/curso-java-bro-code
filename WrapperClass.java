5246af1c7023827ffcccb485fe5b964be1cd73d1
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-02-28 08:40:01 -0300

    Wrapper Class

---
+++ WrapperClass.java
@@ -0,0 +1,11 @@
+public class WrapperClass {
+    public static void main(String[] args) {
+        Boolean a = true;
+        Character b = 'B';
+        Integer c = 123;
+        Double d = 3.14;
+        String e = "Gui";
+
+        System.out.println(c.doubleValue());
+    }
+}
