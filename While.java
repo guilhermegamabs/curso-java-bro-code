a517326b18ab3ff2cbd2e0228d84ad5308eaeb45
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-02-28 07:28:14 -0300

    While Loop

---
+++ While.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class While {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+
+        while(name.isBlank()) {
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+        }
+
+        do {
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+        } while(name.isBlank());
+
+        System.out.println("Hello " + name);
+    }
+}
