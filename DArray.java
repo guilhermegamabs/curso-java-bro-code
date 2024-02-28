598ee29bdcb6ecc30e01a1e71137aad2f79263e5
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-02-28 08:11:57 -0300

    2D Array

---
+++ DArray.java
@@ -0,0 +1,17 @@
+public class DArray {
+    public static void main(String[] args) {
+        // 2DArray
+        String[][] cars = {
+                {"Camaro", "Corvette", "Silverado"},
+                {"Mustang", "Ranger", "F-150"},
+                {"Ferrari", "Lambo", "Tesla"}
+        };
+
+        for (int i=0; i < cars.length; i++) {
+            System.out.println();
+            for (int j = 0; j<cars[i].length;j ++) {
+                System.out.println(cars[i][j]+ " ");
+            }
+        }
+    }
+}
