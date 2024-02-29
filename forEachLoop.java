3cd835e7a0390dbf20011391f329249e02a1833c
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-02-29 20:44:33 -0300

    Add files via upload

---
+++ forEachLoop.java
@@ -0,0 +1,9 @@
+public class forEachLoop {
+    public static void main(String[] args) {
+        String[] animals = {"cat", "dog", "horse"};
+
+        for(String i : animals) {
+            System.out.println(i);
+        }
+    }
+}
