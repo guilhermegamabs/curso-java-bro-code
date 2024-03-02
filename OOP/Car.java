dbc295cd392ed4284785512368dfb34f18b61c17
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-03-02 15:19:19 -0300

    Add files via upload

---
+++ OOP/Car.java
@@ -0,0 +1,17 @@
+package OOP;
+
+public class Car {
+    String make = "Porsche";
+    String model = "Porsche 911 Carrera GTS";
+    int year = 2023;
+    String color = "red";
+    double price = 5500000.00;
+
+    void drive() {
+        System.out.println("You drive the car");
+    }
+
+    void brake() {
+        System.out.println("You step on the brakes");
+    }
+}
---
+++ OOP/ObjectsOOP.java
@@ -0,0 +1,9 @@
+package OOP;
+
+public class ObjectsOOP {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.drive();
+        System.out.println(car1.model);
+    }
+}
