6e5c474f3aa5072d18c05ea81a77021a74f42138
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-03-02 15:03:32 -0300

    PrintF

---
+++ Printf.java
@@ -0,0 +1,12 @@
+public class Printf {
+    public static void main(String[] args) {
+    boolean myBoolean = true;
+    char myChar = 'A';
+    String myString = "Gui";
+    int myInt = 50;
+    double myDouble = 1000;
+
+
+        System.out.printf("%s", myString);
+    }
+}
