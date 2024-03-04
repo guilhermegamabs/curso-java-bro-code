0739792b402f2cbfb06e14c388a3fd2ab5596a1f
Guilherme Gama <109814304+guilhermegamabs@users.noreply.github.com>
2024-03-04 20:47:46 -0300

    Abstraction

---
+++ Abstraction/Main.java
@@ -0,0 +1,10 @@
+package Abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        //Vehicle veh = new Vehicle();
+
+        car1.go();
+    }
+}
